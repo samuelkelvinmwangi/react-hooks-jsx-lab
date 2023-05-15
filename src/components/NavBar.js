8aaedbbc8ed802e20a12c6ce81128cbf3d23587a
samuel <samuelkelvinmwangi@gmail.com>
2023-05-15 19:03:17 +0300

    working in the example

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,14 @@
 import React from "react";
 
+
 function NavBar() {
-  // update the JSX being returned!
-  return <nav>NavBar</nav>;
+ 
+  return 
+    <nav>
+      <a href="#home"></a>
+      <a href="#about"></a>
+    </nav>
+  
 }
 
 export default NavBar;
